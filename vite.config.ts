6a1ca9a69c35221380111c1c5406dac8197819ae
Lakshan Madushanka <epmadushanka@gmail.com>
2024-11-24 14:02:34 +0530

    WIP

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
   plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src') //fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
   build: {
