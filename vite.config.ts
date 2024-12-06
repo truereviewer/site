a18f8c7440e3764f5530feed4d203a1b45b77bf0
Lakshan Madushanka <epmadushanka@gmail.com>
2024-12-06 16:58:28 +0530

    Change build directory to dist

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,4 @@
 import { fileURLToPath, URL } from 'node:url'
-import path from 'path'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
@@ -14,6 +13,6 @@ export default defineConfig({
     }
   },
   build: {
-    outDir: './docs'
+    outDir: './dist'
   }
 })
