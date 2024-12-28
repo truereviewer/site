689884faa7fea06999daa75b74b7d3fc6e7207bd
Lakshan Madushanka <epmadushanka@gmail.com>
2024-12-29 00:28:09 +0530

    wip

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,11 +1,14 @@
 const imgUrl = (name: string, path: string = '', type: 'img' | 'svg' | 'icons' = 'img'): string => {
-  let tPath = `assets/${type}`
+  let tPath = `./assets/${type}`
 
   if (path) {
     tPath = tPath + '/' + path
   }
 
   tPath = tPath + '/' + name
+
+  console.log(tPath)
+  console.log(new URL(tPath, import.meta.url).href)
 
   return new URL(tPath, import.meta.url).href
 }
