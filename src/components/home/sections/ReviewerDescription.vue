50b932065db735907e1e0aae80a23d067843dff5
Lakshan Madushanka <epmadushanka@gmail.com>
2025-01-13 12:32:28 +0530

    WIP

--- src/components/home/sections/ReviewerDescription.vue
+++ src/components/home/sections/ReviewerDescription.vue
@@ -3,11 +3,11 @@
     <template #header>Complete Review System </template>
     <template #content>
       <p class="!mb-0">
-        This review system is thoughtfully crafted to capture users' attention while maintaining a
-        simple, intuitive design. Each component is styled to stand out without overwhelming,
-        creating a smooth user experience. Plus, you have the flexibility to integrate and use each
-        component independently, tailoring the setup to fit seamlessly with your application’s
-        requirements.
+        Truereviewer is a review system thoughtfully crafted to capture users' attention while
+        maintaining a simple, intuitive design. Each component is styled to stand out without
+        overwhelming, creating a smooth user experience. Plus, you have the flexibility to integrate
+        and use each component independently, tailoring the setup to fit seamlessly with your
+        application’s requirements.
       </p>
       <!-- <p class="!my-0">Each component has follwoing constraints</p> -->
     </template>
