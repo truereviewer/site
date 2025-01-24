d11ada59e33510c272d240a48473887a3e929840
Lakshan Madushanka <epmadushanka@gmail.com>
2025-01-24 18:05:28 +0530

    add more to description

--- src/components/home/sections/ReviewerDescription.vue
+++ src/components/home/sections/ReviewerDescription.vue
@@ -9,7 +9,23 @@
         experience. With the flexibility to integrate and utilize components independently,
         TrueReviewer effortlessly adapts to your application’s unique requirements.
       </p>
-      <!-- <p class="!my-0">Each component has follwoing constraints</p> -->
+      <p>Did you know ?</p>
+      <blockquote
+        class="!mt-2"
+        cite="https://www.businesswire.com/news/home/20241009362692/en/New-Survey-Reveals-the-Strong-Influence-of-Online-Reviews-on-Consumer-Decision-Making#:~:text=According%20to%20the%20findings%2C%2054,to%20be%20%E2%80%9Cmostly%E2%80%9D%20trustworthy."
+      >
+        <span
+          >54% of consumers are putting their trust in online reviews first, outweighing the
+          opinions of friends and family (24%), company claims (18%), social media influencers (2%),
+          and media reviews (2%). Notably, 77% of respondents stated that they find online reviews
+          to be “mostly” trustworthy.
+        </span>
+        <a
+          class="flex justify-end font-normal text-sm"
+          href="https://www.businesswire.com/news/home/20241009362692/en/New-Survey-Reveals-the-Strong-Influence-of-Online-Reviews-on-Consumer-Decision-Making#:~:text=According%20to%20the%20findings%2C%2054,to%20be%20%E2%80%9Cmostly%E2%80%9D%20trustworthy."
+          >businesswire</a
+        >
+      </blockquote>
     </template>
   </SectionComponent>
 </template>
