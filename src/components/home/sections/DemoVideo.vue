b620874f82e1feb88cade85e66e985ee93f603af
Lakshan Madushanka <epmadushanka@gmail.com>
2025-09-21 10:02:35 +0530

    wip

--- src/components/home/sections/DemoVideo.vue
+++ src/components/home/sections/DemoVideo.vue
@@ -1,10 +1,12 @@
 <template>
-  <SectionComponent class="flex flex-col items-start justify-center">
-    <template #header>Add Review Functionality in Minutes </template>
+  <SectionComponent
+    class="flex flex-col items-start justify-center bg-gradient-to-b from-green-600 via-yellow-700/40 via-[50%] to-green-600"
+  >
     <template #content>
+      <h3 class="!mt-0 !text-emerald-950 italic">Add Review Functionality in Minutes</h3>
       <div class="w-full">
         <iframe
-          class="border w-full lg:w-2/3 aspect-video p-1"
+          class="w-full lg:w-2/3 aspect-video p-1"
           src="https://www.youtube.com/embed/-NJszz8lnuA?si=Sl8HGlVcG3zYDgsl"
           title="YouTube video player"
           frameborder="0"
